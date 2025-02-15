d3170160455ba1f8734ff54d2614750067f310c1
JoseErisvaldo <joseerisvaldopg@gmail.com>
2025-02-15 00:39:58 -0300

    Fix: type in login

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -4,12 +4,15 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
 
-export default async function Login(props: { searchParams: Promise<Message> }) {
-  const searchParams = await props.searchParams;
+// Alterando o tipo de props para ser o esperado sem Promise
+export default function Login({ searchParams }: { searchParams: Record<string, string | undefined> }) {
   return (
     <div className="flex flex-row gap-12 items-center justify-center">
       <div className="w-full flex-1 flex items-center h-screen sm:max-w-md justify-center gap-2 p-4">
-        <img src="https://img.freepik.com/vetores-gratis/servico-de-entrega-ilustrado_23-2148505081.jpg?t=st=1739501575~exp=1739505175~hmac=1f3f2b13848b062a4a61d757ebd1e917a26056470ce815c0a2748d502a13bd80&w=826" alt="" />
+        <img
+          src="https://img.freepik.com/vetores-gratis/servico-de-entrega-ilustrado_23-2148505081.jpg?t=st=1739501575~exp=1739505175~hmac=1f3f2b13848b062a4a61d757ebd1e917a26056470ce815c0a2748d502a13bd80&w=826"
+          alt="Illustration"
+        />
       </div>
       <form className="flex flex-col min-w-64 max-w-64 mx-auto border border-foreground/10 p-4 rounded-md">
         <h1 className="text-2xl font-medium">Entrar</h1>
@@ -24,10 +27,7 @@ export default async function Login(props: { searchParams: Promise<Message> }) {
           <Input name="email" placeholder="exemplo@exemplo.com" required />
           <div className="flex justify-between items-center">
             <Label htmlFor="password">Senha</Label>
-            <Link
-              className="text-xs text-foreground underline"
-              href="/forgot-password"
-            >
+            <Link className="text-xs text-foreground underline" href="/forgot-password">
               Esqueceu a senha?
             </Link>
           </div>
@@ -43,5 +43,5 @@ export default async function Login(props: { searchParams: Promise<Message> }) {
         </div>
       </form>
     </div>
-  );  
+  );
 }
